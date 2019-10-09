1a47471787badaab83c3f939d502b5aff1da3e5e
asapre <asapre@vmware.com>
2019-10-09 13:33:08 -0400

    first commit

---
+++ app/src/main/java/com/temp/myapplication/SampleHelper.java
@@ -0,0 +1,18 @@
+package com.temp.myapplication;
+
+public class SampleHelper {
+
+    /**
+     * <h2> H2 </h2>
+     * <h3> H3 </h3>
+     *
+     * <ul>
+     *     <li>li1</li>
+     *     <li>li2</li>
+     * </ul>
+     * @return version value
+     */
+    public static int getVersion(){
+        return 1;
+    }
+}
